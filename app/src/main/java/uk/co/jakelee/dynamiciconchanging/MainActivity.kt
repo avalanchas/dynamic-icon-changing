7473129c51b183880e022a92a37a70cc18d59538
RadioPatrick <p.heuschkel@radio.net>
2022-11-17 16:41:22 +0100

    update build tools, dependencies and gitignore

--- app/src/main/java/uk/co/jakelee/dynamiciconchanging/MainActivity.kt
+++ app/src/main/java/uk/co/jakelee/dynamiciconchanging/MainActivity.kt
@@ -3,7 +3,7 @@ package uk.co.jakelee.dynamiciconchanging
 import android.content.ComponentName
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
